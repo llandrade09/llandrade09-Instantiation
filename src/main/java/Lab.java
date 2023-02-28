c2226074220d0e26fc6fcbc747c3e0385eef4b47
Luis Andrade <santiago16999@gmail.com>
2023-02-28 06:58:30 +0000

    no Null Object

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Object make = new Object();
+        return make;
     }
 }
